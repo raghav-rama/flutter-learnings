425e5a31f3c33f7eb783caa69ad48c722dd95db5
Ritvik <91389059+raghav-rama@users.noreply.github.com>
2024-06-09 23:36:19 +0530

    Update temperature_simulation.dart

--- streams/temperature_simulation.dart
+++ streams/temperature_simulation.dart
@@ -1,1 +1,14 @@
 
+import "package:http/http.dart" as http;
+import "dart:convert";
+
+void main() async {
+  var url = Uri.https("api.openweathermap.org", "data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}");
+  try {
+    final res = await http.get(url);
+    final jsonResponse = jsonDecode(res.body);
+    print(jsonResponse["current"]["temp"]);
+  } catch (e) {
+    print("Some error occured\n$e");
+  }
+}
