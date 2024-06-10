e91e276d00c933c508224667b8054af58b56fcd6
Ritvik <91389059+raghav-rama@users.noreply.github.com>
2024-06-10 22:50:32 +0530

    Update temperature_simulation.dart

--- streams/temperature_simulation.dart
+++ streams/temperature_simulation.dart
@@ -1,14 +1,47 @@
+import 'dart:async';
+import 'dart:math';
 
-import "package:http/http.dart" as http;
-import "dart:convert";
+Future<int> fetchCurrentTemperature() {
+  return Future.delayed(Duration(seconds: 1), () {
+    final random = Random();
+    return random.nextInt(51) - 10;
+  });
+}
+
+Stream<int> streamTemperatureUpdates() {
+  return Stream.periodic(Duration(seconds: 1), (_) {
+    final random = Random();
+    return random.nextInt(51) - 10;
+  }).take(10);
+}
 
 void main() async {
-  var url = Uri.https("api.openweathermap.org", "data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}");
-  try {
-    final res = await http.get(url);
-    final jsonResponse = jsonDecode(res.body);
-    print(jsonResponse["current"]["temp"]);
-  } catch (e) {
-    print("Some error occured\n$e");
-  }
+  print('Fetching current temperature...');
+  final currentTemperature = await fetchCurrentTemperature();
+  print('Current temperature: $currentTemperature°C');
+
+  final temperatureStream = streamTemperatureUpdates();
+  final subscription = temperatureStream.listen((temperature) {
+    print('Temperature update: $temperature°C');
+  });
+
+  await subscription.asFuture();
+  print('Temperature monitoring stopped.');
 }
+
+/*
+### OUTPUT ###
+Fetching current temperature...
+Current temperature: 15°C
+Temperature update: 0°C
+Temperature update: 19°C
+Temperature update: 39°C
+Temperature update: 18°C
+Temperature update: 28°C
+Temperature update: 24°C
+Temperature update: 19°C
+Temperature update: 0°C
+Temperature update: 2°C
+Temperature update: 29°C
+Temperature monitoring stopped.
+*/
